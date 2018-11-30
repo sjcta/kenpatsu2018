7830a3dbe5a357ce728908813a75ac0b2b284807
sjcta <x3arts.cn@gmail.com>
2018-11-30 16:22:31 +0800

    1.43 + lunchdinner

--- src/router.js
+++ src/router.js
@@ -1,29 +1,35 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import index from './views/index.vue'
-import agenda from './views/agenda.vue'
-import panel from './views/panel.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import index from "./views/index.vue";
+import agenda from "./views/agenda.vue";
+import panel from "./views/panel.vue";
+import lunchDinner from "./views/lunch.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'indexPage',
+      path: "/",
+      name: "indexPage",
       component: index
     },
     {
-      path: '/agenda',
-      name: 'agendaPage',
+      path: "/agenda",
+      name: "agendaPage",
       component: agenda
     },
     {
-      path: '/panel',
-      name: 'panelList',
+      path: "/panel",
+      name: "panelList",
       component: panel
+    },
+    {
+      path: "/lunchdinner",
+      name: "lunchDinner",
+      component: lunchDinner
     }
   ]
-})
+});
---
+++ src/views/lunch.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <headerBar v-on:childLang="$emit('changeLang')">{{ $vuetify.t('$vuetify.index.lunchDinner') }}</headerBar>
+
+    <div id="lunchDinner">
+      <h1>lunch</h1>
+
+      <h1>Dinner</h1>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+import headerBar from "../components/headerBar.vue";
+
+export default {
+  components: {
+    headerBar
+  }
+};
+</script>
