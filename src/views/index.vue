a02ce8e8fd02ffc1064e351f3a6557ee1ba6a867
sjcta <x3arts.cn@gmail.com>
2018-11-30 18:53:47 +0800

    1.45

--- src/views/index.vue
+++ src/views/index.vue
@@ -93,7 +93,7 @@
     </v-content>
 
     <v-footer class="pa-3" light>
-      <v-flex text-xs-center>&copy;{{ new Date().getFullYear() }} HCR&amp;D - V1.44a</v-flex>
+      <v-flex text-xs-center>&copy;{{ new Date().getFullYear() }} HCR&amp;D - V1.45</v-flex>
     </v-footer>
   </div>
 </template>
--- src/views/lunch.vue
+++ src/views/lunch.vue
@@ -4,23 +4,74 @@
 
     <v-content id="panelList">
       <v-container fluid>
-        <v-card color="blue-grey darken-2" class="white--text">
+        <v-card color="white" class="lunch">
           <v-card-title primary-title>
             <div>
-              <div class="headline text-xs-center">{{ $vuetify.t('$vuetify.lunchDinner.lunch') }}</div>
-              <span class="text-xs-center grey--text">12:00 - 13:30</span>
+              <div class="headline white--text">{{ $vuetify.t('$vuetify.lunchDinner.lunch') }}</div>
+              <span class=" white--text">12:00 - 13:30</span>
             </div>
           </v-card-title>
           <v-card-actions>
-            <v-btn flat dark>Listen now</v-btn>
+            <div class="d-flex"> 
+              <v-icon large color="darken-2">location_on</v-icon>
+              <span>Wenjin Hotel 2F Reno Café (buffet)</span>
+            </div>
           </v-card-actions>
         </v-card>
+
+        <v-card color="white" class="dinner">
+          <v-card-title primary-title>
+            <div>
+              <div class="headline white--text">{{ $vuetify.t('$vuetify.lunchDinner.dinner') }}</div>
+              <span class=" white--text">18:30 ~ 20:00</span>
+            </div>
+          </v-card-title>
+          <v-card-actions>
+            <div class="d-flex"> 
+              <v-icon large color="dark-1">location_on</v-icon>
+              <span>Wenjin Hotel 5F</span>
+            </div>
+          </v-card-actions>
+        </v-card>
+
+
       </v-container>
     </v-content>
   </div>
 </template>
 
 <style scoped>
+.v-card {
+  margin-bottom: 20px;
+}
+.v-card__title {
+  align-items: flex-start;
+  height: 150px;
+  padding-left: 50px;
+}
+.v-card.lunch .v-card__title {
+  background: #FFF url('../assets/bg_lunch.png') no-repeat right top;
+  background-size: 100% auto;
+}
+.v-card.dinner .v-card__title {
+  background: #FFF url('../assets/bg_dinner.png') no-repeat right top;
+  background-size: 100% auto;
+}
+.v-card__title div {
+  width: 100%;
+}
+.v-card__actions {
+  padding-bottom: 30px;
+  justify-content:flex-start;
+}
+.v-card__actions .d-flex {
+  justify-content: center;
+  align-content: center;
+  padding-left: 10px;
+}
+.v-card__actions span {
+  line-height: 3;
+}
 </style>
 
 <script>
